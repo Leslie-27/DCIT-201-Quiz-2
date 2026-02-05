eebc59bd117b0b1f49115a5f70ae4564a1001693
Leslie <ldnormanyo@st.ug.edu.gh>
2026-02-04 17:11:13 -0800

    Update and rename Answers to Quiz 2 to Question1.executeGradeReport.java

--- Question1.executeGradeReport.java
+++ Question1.executeGradeReport.java
@@ -1,4 +1,4 @@
- QUESTION 1
+
 public class Main {
     public static void main(String[] args) {
         executeGradeReport(0);
@@ -25,27 +25,6 @@ public class Main {
         System.out.println("Final feedback displayed");
     }
 
-QUESTION 2
-public class Main {
-    public static void main(String[] args) {
-
-    }
-    public static double calculateClassAverage(double score1, double score2) {
-        double result = (score1 + score2) / 2;
-        return result
-
-    }
-
-    public static double calculateClassAverage(double score1, double score2, double score3) {
-        double result = (score1 + score2 + score3) / 3;
-        return result
-
-    }
-
-    public static double calculateClassAverage(double[] scores) {
-        double sum = 0;
-        for (int i = 0; i < scores.length; i++) {
-            sum += scores[i];
         }
         return sum / scores.length;
 
